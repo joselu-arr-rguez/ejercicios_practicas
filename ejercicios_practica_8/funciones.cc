a9b6fe5d576fd39c60c9a23f9ac8a40e847d7084
usuario <jose.luis.26@ull.edu.es>
2020-12-01 22:55:37 +0000

    Última actualización (01-12-2020)

--- ejercicios_practica_8/funciones.cc
+++ ejercicios_practica_8/funciones.cc
@@ -25,31 +25,31 @@
 */
 
 
-// We have included the library (math.h) to be able to use the function (pow).
+// We have included the library "math.h" to be able to use the function "pow".
 
 #include <math.h>
 
 
-// We initialize the variables that we are going to use, both integers (int).
+// We declare the variables that we are going to use, both variables of the type "int".
 
-// The function will return a value of the integer type (int), then we pass it as an input parameter a value of the integer type (int) which we call "number".
+// The function will return a value of the variable of type "int", then we pass it as an input parameter to a value of the variable of type "int" that we call "number".
 
 int SumSquares (int number) 
 
 {
+      
   
-  
-  // We declare a variable of the type integer (int), which we call "sum" and we initialize to (0).
+  // We declare a variable of type "int", which we call "sum" and we declare it to (0).
   
   int sum = 0;
   
   
-  // We declare a variable of the type integer (int), which we call "square".
+  // We declare a variable of type "int", which we call "square".
   
   int square;
   
   
-  // We declare a (for) which will collect the numbers that are less than the variable of the integer type (int) called "number" until it reaches this, which will add the squares of the numbers.
+  // Declaramos un bucle "for", que recogerá los números que sean menores que la variable "int" (llamada "número") hasta llegar a este, el cuál sumará los cuadrados de los números.
   
   for (int i = 1; i <= number; i++) 
   
@@ -64,75 +64,100 @@ int SumSquares (int number)
     
   }
   
-
+  
   // We return the "sum" variable since the function returns an integer.
-
+  
   return sum;
-
-
+  
+  
 }
 
+ 
+// We declare the variables that we are going to use, both variables (int).
 
-// We initialize the variables that we are going to use, both integers (int).
+int SquareSum (int number) 
 
-int SquareSum (int number) {
-
-
+{
+  
+  
   // We declare a variable of the type integer (int), which we call "sum" and we initialize to (0).
-
+  
   int sum = 0;
-
-
+  
+  
   // We declare a variable of the type integer (int), which we call "square".
-
+  
   int square;
-
-
+  
+  
   // We declare a (for) and add all the numbers, and then square the result. Returning as the last result the result of the square.
-
+  
   for (int i = 1; i <= number; i++)
-
+  
   {
-
+  
   sum += i;
-
+  
     
   // We declare the function (pow) that says that the first number is the base of the exponent and the second number, in this case (2), is the exponent.
-
+  
   square = pow (sum, 2);
-
+  
   }
-
-
+  
+  
   // We return the "square" variable since the function returns an integer.
-
+  
   return square;
-
-
+  
+  
 }
 
 
-double Average (int N, double vector []) {
+// We declare the average function, to which we pass the size of the vector (N) as an input parameter, using a variable (int) and the vector (N) itself. This function will return a variable (double), which will be the result of the average.
 
-// Declaramos la funcion media a la que le pasamos por parametro de entrada el tamaño del vector (N) y el propio vector (N). Esta funcion nos devolvera un double que sera el resultada de la media
-double average;
+double Average (int N, double vector []) 
 
-double sum;
-
-// Utilizaremos un for para recorrer el vector posicion por posicion, dentro del for guardaremos en la variable suma el resultado de semar todos los elemntos del vector
-for (int i = 0; i < N; i++)
 {
+  
+  
+  // We declare a variable (double), which we call "average".
+  
+  double average;
+  
+  
+  // We declare a variable (double), which we call "sum".
+  
+  double sum;
+  
+
+  // We declare a loop (for), to go through the vector position by position, inside it, we save in the variable (int) called "sum", the result of adding all the elements of the vector.
+  
+  for (int i = 0; i < N; i++)
+  
+  {
+  
   sum += vector [i]; 
+  
+  }
+  
+  
+  // Outside the (for) loop we carry out the average operation, and store it in the variable (double) which we had called "average" (with the same name).
+  
+  average = sum / N;
+  
+  
+  // We declare a statement (return), which returns the result of the previously made average.
+  
+  return average;
+  
+  
 }
-average = sum / N;
-// Fuera del for realizamos la operacion media y la guardamos en la variabl econ el mismo nombre y devolvemos con un return el resultado de la media 
-return average;
 
 
-
-}
 // En la funcion Find Higher le pasamos el tamaño del vector asi como el propio vector y buscaremos entre esos elementos el elemento mayor devolviendolo como mreturn
-double FindHigher (int N,double vector []) {
+
+double FindHigher (int N, double vector []) {
 // Guardamos en Highre el primer elemento del vector para mas adelante poder hacer comporaraciones
 double higher = vector [0];
 // Utilixzaremos el for para recorrer el vector pero estavez dede la posicion 1 ya que la posicion 0 se encuentra guardada en la variable higher
