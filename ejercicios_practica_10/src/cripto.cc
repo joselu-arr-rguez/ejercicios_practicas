5f8133f7416b2ae37e19bc243307d37403dfe7f1
usuario <jose.luis.26@ull.edu.es>
2021-01-23 14:17:22 +0000

    Actualizado

--- ejercicios_practica_10/src/cripto.cc
+++ ejercicios_practica_10/src/cripto.cc
@@ -4,12 +4,9 @@
  * Grado en Ingeniería Informática
  * Informática Básica
  *
- * @author F. de Sande
- * @date 7.nov.2020
- * @brief Cada nuevo término de la serie de Fibonacci se genera sumando los dos anteriores. 
- *        Comenzando con 0 y 1, los primeros 10 términos serán: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
- *        Desarrolle en C++ un programa que calcule la suma de todos los términos de valor par 
- *        de la serie que sean menores que 1000.
+ * @author José Luis
+ * @date 22 de Enero de 2021
+ * @brief main que llama a un menú para poder realizar distintos tipos de cifrado.
  * @see https://docs.google.com/document/d/1-3hTIVf8tPrbn9u0vs0Cm2IGyX1XBgv8hReVU0KOSUQ/edit?usp=sharing
  * @see stoi http://www.cplusplus.com/reference/string/stoi/
  * An Object Oriented Version of the program:
